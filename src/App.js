a891a289c3ca6528e1dbe4c9d5d28bd78e205596
oArthur <arthursouzamaciel@gmail.com>
2023-07-25 10:02:32 -0300

    Adicionando fireworks onClick

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import ContainerDaysLeft from './components/ContainerDaysLeft'
 import Counter from './components/Counter'
+import Fireworks from './components/Fireworks'
 import './styles/components/app.sass'
 
 import useCountdown from './hooks/useCountdown';
 function App() {
   const [day,hour,minute,second] = useCountdown("Jul 28, 2023 08:30:00");
   return (
-    <div id='principal'>
-      <h2>&#10024; Trip to Brasilia &#10024;</h2>
-      <ContainerDaysLeft dias={day} />
-      <Counter title="hours" number={hour}/>
-      <Counter title="minutes" number={minute}/>
-      <Counter title="seconds" number={second}/>
-
-    </div>
+    <>
+      <div id='principal'>
+        <h2><Fireworks/> Trip to Brasilia &#10024;</h2>
+        <ContainerDaysLeft dias={day} />
+        <Counter title="hours" number={hour}/>
+        <Counter title="minutes" number={minute}/>
+        <Counter title="seconds" number={second}/>
+      </div>
+      
+    </>
+    
   );
 }
 
