7f0394208d9ad9479c5b362e11e4b2fb6e7d1adf
Arthur Maciel <50220461+oArthur@users.noreply.github.com>
2023-11-14 10:05:27 -0300

    new date

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './styles/components/app.sass'
 
 import useCountdown from './hooks/useCountdown';
 function App() {
-  const [day,hour,minute,second] = useCountdown("Nov 10, 2023 21:30:00");
+  const [day,hour,minute,second] = useCountdown("Nov 24, 2023 19:00:00");
   return (
     <>
       <div id='principal'>
-        <h2 id="title"><Fireworks/> Trip to SP</h2>
+        <h2 id="title"><Fireworks/> Trip to BSB bday🎈</h2>
         <ContainerDaysLeft dias={day} />
         <Counter title="hours" number={hour}/>
         <Counter title="minutes" number={minute}/>
